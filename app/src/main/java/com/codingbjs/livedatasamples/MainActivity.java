9830bbc7478a65cb580a41484259192489cd32a9
CDBJS <codingbjs@gmail.com>
2022-02-09 14:14:08 +0900

    Room Database 생성

--- app/src/main/java/com/codingbjs/livedatasamples/MainActivity.java
+++ app/src/main/java/com/codingbjs/livedatasamples/MainActivity.java
@@ -1,14 +1,39 @@
 package com.codingbjs.livedatasamples;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.codingbjs.livedatasamples.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mainBinding;
+
+    AppDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
+
+        db = Room.databaseBuilder(this, AppDatabase.class, "todo_db")
+                .allowMainThreadQueries()
+                .build();
+
+        mainBinding.resultText.setText(db.todoDao().getAll().toString());
+
+        mainBinding.addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                db.todoDao().insert(new Todo(mainBinding.todoEditText.getText().toString()));
+                mainBinding.resultText.setText(db.todoDao().getAll().toString());
+            }
+        });
+
+
     }
 }
